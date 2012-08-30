75039abff8815a0f168162bd44297140f4ef8347
Eduardo Gutierrez <egutierrez@zipcar.com>
2012-08-29 20:49:42 -0400

    Add more descriptive example blocks

--- spec/classes/ruby_spec.rb
+++ spec/classes/ruby_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe 'ruby' do
-  it { should contain_class('ruby::ruby_build') }
-  it { should contain_class('ruby::packages') }
+  it "installs packages necessary for building ruby" do
+    should contain_class('ruby::packages')
+  end
+
+  it "adds packages before ruby-build" do
+    should contain_class('ruby::ruby_build')
+  end
 
   it "adds ruby.sh profile script" do
     should contain_file('ruby.sh').with(
